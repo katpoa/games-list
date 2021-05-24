0e8aa7ccce7acccbae3844efb6e570db3b2f265a
Katie Poa <pooka@Katies-MacBook-Pro.local>
2021-05-24 01:44:07 -0700

    Changes for heroku

--- server/index.js
+++ server/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const axios = require('axios');
 
 // const controller = require('./controllers/controller');
-const { client_id, client_secret } = require('../config.js');
+// const { client_id, client_secret } = require('../config.js');
 
 
 const app = express();
-const port = 3000;
+const port = 8080;
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -21,9 +21,9 @@ app.use(cors());
 
 app.get('/api/', async (req, res) => {
   try {
-    const response = await axios.post(`https://id.twitch.tv/oauth2/token?client_id=${client_id}&client_secret=${client_secret}&grant_type=client_credentials`);
+    const response = await axios.post(`https://id.twitch.tv/oauth2/token?client_id=0d2pau6mvwsnjgb0wntjitxiivs8wu&client_secret=bcdv8z1ev3wq2n5f5t87n96hnl8spa&grant_type=client_credentials`);
     if (!response) { console.log('Error: couldnt get token' ); return res.sendStatus(500) }
-    const response1 = await axios.post('https://api.igdb.com/v4/games/', 'fields *;', { headers: { 'Client-ID': client_id, 'Authorization': `Bearer ${response.data.access_token}` }});
+    const response1 = await axios.post('https://api.igdb.com/v4/games/', 'fields *;', { headers: { 'Client-ID': '0d2pau6mvwsnjgb0wntjitxiivs8wu', 'Authorization': `Bearer ${response.data.access_token}` }});
     if (!response1) { console.log('Error: couldnt get data' ); return res.sendStatus(500)}
     res.status(200).send(response1.data);
   } catch(e) {
@@ -35,9 +35,9 @@ app.get('/api/', async (req, res) => {
 app.get('/api/covers', async (req, res) => {
   const { id } = req.params;
   try {
-    const response = await axios.post(`https://id.twitch.tv/oauth2/token?client_id=${client_id}&client_secret=${client_secret}&grant_type=client_credentials`);
+    const response = await axios.post(`https://id.twitch.tv/oauth2/token?client_id=0d2pau6mvwsnjgb0wntjitxiivs8wu&client_secret=bcdv8z1ev3wq2n5f5t87n96hnl8spa&grant_type=client_credentials`);
     if (!response) { console.log('Error: couldnt get token' ); return res.sendStatus(500) }
-    const art = await axios.post('https://api.igdb.com/v4/covers/', 'fields url; where id = ' + id,  { headers: { 'Client-ID': client_id, 'Authorization': `Bearer ${response.data.access_token}` }});
+    const art = await axios.post('https://api.igdb.com/v4/covers/', 'fields url; where id = ' + id,  { headers: { 'Client-ID': '0d2pau6mvwsnjgb0wntjitxiivs8wu', 'Authorization': `Bearer ${response.data.access_token}` }});
     if (!art) { console.log('Error: couldnt get cover artwork'); return res.sendStatus(500)}
     res.status(200).send(art.data);
   } catch(e) {
